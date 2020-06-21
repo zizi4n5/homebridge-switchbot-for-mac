6d9464d1a390228a09f97a0de63e1ebabf489255
zizi4n5 <zizi4n5@gmail.com>
2020-06-21 23:52:08 +0900

    fix: Added support for setting PLUGIN_NAME to Accessory.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
+import { PLUGIN_NAME, PLATFORM_NAME } from './settings';
 import { SwitchBotAccessory } from './accessory'; 
 
 /*
  * This method registers the accessory with Homebridge
  */
 export default function (api: API): void {
-  api.registerAccessory(PLATFORM_NAME, SwitchBotAccessory);
+  api.registerAccessory(PLUGIN_NAME, PLATFORM_NAME, SwitchBotAccessory);
 }
